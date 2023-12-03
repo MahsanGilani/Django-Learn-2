4f881fc1a0c30ec21a31eafa3ae15f2e9fb696b2
MahsanGilani <mahsan_gillani@yahoo.com>
2023-12-03 20:20:46 +0330

    using models in views and post template edited

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
+
 
 # Create your views here.
 def post_list(request):
     # return HttpResponse('hello')
-    return render(request, 'posts/post_list.html')
+    posts = Post.objects.all()
+    print(posts)
+    return render(request, 'posts/post_list.html',{'posts': posts})
