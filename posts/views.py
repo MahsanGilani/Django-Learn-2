8fda5b5183d04db5d2bb181035620f8eb2ec47ae
MahsanGilani <mahsan_gillani@yahoo.com>
2023-12-17 09:21:26 +0330

    views added for comment

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Comment
 
 
 # Create your views here.
@@ -8,3 +8,8 @@ def post_list(request):
     posts = Post.objects.all()
     # print(posts)
     return render(request, 'posts/post_list.html',{'posts': posts})
+
+def comment_list(request):
+    comment = Comment.objects.all()
+    return render(request, 'posts/comment_list.html',{'comment': comment})
+    
