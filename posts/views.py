737e6fb3c31499f59c3864691f8f745e6fcbcf88
MahsanGilani <mahsan_gillani@yahoo.com>
2023-12-17 09:50:23 +0330

    views for post detail has been created

--- posts/views.py
+++ posts/views.py
@@ -12,4 +12,7 @@ def post_list(request):
 def comment_list(request):
     comment = Comment.objects.all()
     return render(request, 'posts/comment_list.html',{'comment': comment})
-    
+
+def post_detail(request, id):
+    post = Post.objects.get(pk=id)
+    return render(request, 'posts/post_detail.html',{'post': post})
