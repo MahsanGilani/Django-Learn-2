03cf66d7ff79e42a9da04f739d824db6bec9a5c8
MahsanGilani <mahsan_gillani@yahoo.com>
2023-12-17 09:49:56 +0330

    urls for post detail created

--- posts/urls.py
+++ posts/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list),
-    path('comment/', views.comment_list),
+    # path('comment/', views.comment_list),
+    # path('detail/<int:id>', views.post_detail),
+    path('<int:id>/', views.post_detail),
 ]
